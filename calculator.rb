390b609714d17191b11f9c32c04d7ddb6a3d5eb3
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-14 14:54:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 
 first_number = 1.5 
-second_number = 2
+second_number = 0
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
