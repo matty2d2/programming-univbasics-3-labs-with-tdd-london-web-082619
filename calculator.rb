23f5d348bf8e8e24eee35f43960ba0f0c2cbc417
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-14 14:57:16 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+
+first_number = 1.5 
+second_number = 2
+sum = first_number + second_number
+difference = first_number - second_number   #order of subtraction evaluates the result to be -ve/+ve
+product = first_number * second_number
+quotient = first_number / second_number
