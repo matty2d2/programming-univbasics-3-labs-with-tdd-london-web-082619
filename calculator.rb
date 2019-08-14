f570df7509b2689659a15542e15f80142927dfed
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-14 14:53:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,5 @@ first_number = 1.5
 second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
-product
+product = first_number * second_number
+quotient = first_number / second_number
