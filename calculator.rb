a34a8f73a8b69585dc4b345baad742752ab93ac6
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-14 14:57:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,6 @@
 first_number = 1.5 
 second_number = 2
 sum = first_number + second_number
-difference = first_number - second_number 
+difference = first_number - second_number   #order of subtraction evaluates the result to be -ve/+ve
 product = first_number * second_number
 quotient = first_number / second_number
